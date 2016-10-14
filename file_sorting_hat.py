15604a91cb546b64841ce0683cd1947c6dbf4ade
Jetoff <johnflgn@gmail.com>
2016-10-13 21:35:16 -0400

    FSH - Added remove stopwords function and cleaned up comments

--- file_sorting_hat.py
+++ file_sorting_hat.py
@@ -8,16 +8,16 @@ from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
 from pdfminer.layout import LAParams
 from cStringIO import StringIO
 import string
+from nltk.corpus import stopwords
 import nltk
 
-def pdf_parser(data):
+en_stopwords = stopwords.words('english')
 
+def pdf_parser(data):
     fp = file(data, 'rb')
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    device = TextConverter(rsrcmgr, retstr, codec='utf-8', laparams=(LAParams()))
     # Create a PDF interpreter object.
     interpreter = PDFPageInterpreter(rsrcmgr, device)
     # Process each page contained in the document.
@@ -26,20 +26,14 @@ def pdf_parser(data):
         interpreter.process_page(page)
         data =  retstr.getvalue()
 
-    output =  remove_punctuation(nltk.word_tokenize(data.decode('utf-8')))
+    return process_file_string(data)
 
-    return output
-
-    # output = data.split()
-    #
-    # # Using clean_word function to clean each word in the list
-    # for w in range(len(output)):
-    #     output[w] = clean_word(output[w])
-    # # Removing all empty strings from list
-    # while '' in output:
-    #     output.remove('')
-    #
-    # return output
+def process_file_string(data):
+    data = data.lower()
+    data = nltk.word_tokenize(data.decode('utf-8'))
+    data = remove_punctuation(data)
+    data = remove_stopwords(data)
+    return data
 
 def remove_punctuation(s):
     result = []
@@ -49,16 +43,12 @@ def remove_punctuation(s):
 
     return result
 
-# Remove whitespace, punctuation, etc. from a word; turn it into a lower case
-# string with only alphanumeric characters.
-# def clean_word(word):
-#     if type(word) is str:
-#         return filter(str.isalpha, word).lower()
-#     elif type(word) is unicode:
-#         # Normalize unicode and turn it into ASCII
-#         non_unicode_word = (unicodedata.normalize('NFKD', word).
-#             encode('ascii', 'ignore'))
-#         return filter(str.isalpha, non_unicode_word).lower()
+def remove_stopwords(s):
+    result = []
+    for word in s:
+        if word not in en_stopwords:
+            result.append(word)
+    return result
 
 if __name__ == '__main__':
     #source_file = sys.argv[1]
