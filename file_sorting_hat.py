79c292a0cbcc70644ae987ca59f3e40b400c1f01
Jetoff <johnflgn@gmail.com>
2016-10-13 20:21:45 -0400

    FSH - Initial Commit

---
+++ file_sorting_hat.py
@@ -0,0 +1,33 @@
+__author__ = 'johnfulgoni'
+
+import shutil
+import sys
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.layout import LAParams
+from cStringIO import StringIO
+
+def pdfparser(data):
+
+    fp = file(data, 'rb')
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    # Create a PDF interpreter object.
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    # Process each page contained in the document.
+
+    for page in PDFPage.get_pages(fp):
+        interpreter.process_page(page)
+        data =  retstr.getvalue()
+
+    print data
+
+if __name__ == '__main__':
+    #source_file = sys.argv[1]
+    source_file = 'Data/SampleFinal.pdf'
+    result = pdfparser(source_file)
+    print result
